ff9b5665609289257ca1b6382ef8c7ed3920d622
Roberto Navarro <navarroc.roberto@gmail.com>
2020-06-18 20:22:00 -0500

    Initial Version

---
+++ banco/NewMain.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banco;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ *
+ * @author Roberto Navarro
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)  {
+        Timeporalizadorinador tmp = new Timeporalizadorinador();
+        tmp.run();
+        tmp.stop();
+        
+    }
+}
---
+++ banco/bancoRepository.java
@@ -0,0 +1,92 @@
+package banco;
+
+import Province;
+import java.sql.*;
+import java.util.*;
+ 
+/**
+ * ProvinceRepository: data accessor
+ * @author http://lycog.com
+ */
+
+public class bancoRepository {
+    
+    public static ArrayList showAll() {
+        ArrayList arr = new ArrayList();
+        try {
+            String QRY = "SELECT * FROM companias ORDER BY RFC";
+            Connection con = DBManager.getInstance().getConnection();
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(QRY);
+            while (rs.next()) {
+                Compania compania = new Compania();
+                compania.setRFC(rs.getString("RFC"));
+                compania.setNumAccionesTot(rs.getInt("numAccionesTot"));
+                compania.setNumAccionesDisp(rs.getInt("numAccionesDisp"));
+                compania.setValorActualAccion(rs.getFloat("valorActualAccion"));
+                arr.add(compania);
+            }
+        } catch (SQLException se) {
+            System.out.println(se);
+        }
+        return arr;
+    }
+    
+    public static ArrayList getPortafolio(String RFC) {
+        ArrayList arr = new ArrayList();
+        try {
+            String QRY = "SELECT * FROM usuarios WHERE RFCUsuario = '" + RFC + "'";
+            Connection con = DBManager.getInstance().getConnection();
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(QRY);
+            while (rs.next()) {
+                Transaccion transaccion = new Transaccion();
+                transaccion.setRFCUsuario(rs.getString("RFCUsuario"));
+                transaccion.setRFCComp(rs.getString("RFCComp"));
+                transaccion.setAccionesOperadas(rs.getInt("numAcciones"));
+                transaccion.setPrecioOperacion(rs.getFloat("ultPrecioCompra"));
+                arr.add(transaccion);
+            }
+        } catch (SQLException se) {
+            System.out.println(se);
+        }
+        return arr;
+    }
+    
+    public static void generarTransaccion(Transaccion t) {
+        try {
+            String QRY = "INSERT INTO transacciones (RFCUsuario, RFCComp, fecha,"
+            + " numAccionesOperadas, precioOperacion) values('" + t.getRFCUsuario() + "', '" +
+            t.getRFCComp() + "', '" + t.getFecha() +  "', '" + t.getAccionesOperadas() +
+            "', '" + t.getPrecioOperacion() + "'";
+            Connection con = DBManager.getInstance().getConnection();
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(QRY);
+        } catch(SQLException se){
+            System.out.println(se);
+        }
+        System.out.println("Acción completada");
+    }
+    
+    //Código actualizar número de acciones disponibles de una compañía
+    
+    //Aquí deberá ir el código de hacer una puja
+    
+    //Código para iniciar temporizador
+    
+    //Código para detener temporizador
+    
+    //Código para decidir al ganador de la acción
+    
+    //Código enviar notificación de ganador de compra
+    
+    //Código enviar notificación de rechazo de oferta
+    
+    //código enviar notificación de venta
+    
+    //Actualizar precio por acción
+    
+    //Login
+    
+    
+}
